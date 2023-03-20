b3c5c466ec5c4b70223d08e41fd8052cfe76ca6e
ka1amita <kalamatej@gmail.com>
2023-03-20 18:53:34 +0100

    complete solution

---
+++ Basic building blocks in JavaScript/script.js
@@ -0,0 +1,54 @@
+// let a = 7;
+// if (a < 5) {
+//   console.log(a / 2);
+// } else {
+//   console.log(1);
+// }
+
+// for (let i = 0; i <= 20; i ++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+    
+// }
+
+// Functions
+
+// Exercise 1
+
+// let printNumbersTill = (number) => {
+//     for (let i = 1; i <= number; i++) {
+//         console.log(i)
+//     }
+// }
+// printNumbersTill(15)
+
+// Exercise 2
+
+// let getGreetingTo = (name) => {
+//    return `Hello ${name}!`
+// }
+
+// getGreetingTo('Mark');
+// console.log(getGreetingTo('Mark'))
+
+// For loops and arrays
+
+// Exercise 1
+
+// let printValues = (array) => {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(array[i])
+//     }
+// }
+
+let myArray = [1,5,8,10]
+// printValues(myArray)
+
+// forEach
+
+// Exercise 2
+
+myArray.forEach(number => {
+    console.log(number)  
+});
